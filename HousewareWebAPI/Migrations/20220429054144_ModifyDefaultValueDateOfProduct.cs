8c676c287a95cb3d8b926a580278a2fcb89e1878
trungdungsuoinho <83172493+trungdungsuoinho@users.noreply.github.com>
2022-04-29 12:42:30 +0700

    Modify default value date of product

---
+++ HousewareWebAPI/Migrations/20220429054144_ModifyDefaultValueDateOfProduct.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HousewareWebAPI.Migrations
+{
+    public partial class ModifyDefaultValueDateOfProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "DATEADD(hh, 7, GETUTCDATE())",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "DATEADD(hh, 7, GETUTCDATE())",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "DATEADD(hh, 7, GETUTCDATE())");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "DATEADD(hh, 7, GETUTCDATE())");
+        }
+    }
+}
--- SQLHouseware.sql
+++ SQLHouseware.sql
@@ -1,3 +1,4 @@
+-- Trigger upper Primary Key
 GO
 DROP TRIGGER upper_ClassificationId;
 GO
@@ -25,6 +26,22 @@ END
 GO
 
 GO
+DROP TRIGGER upper_ProductId;
+GO
+CREATE TRIGGER upper_ProductId
+ON Products
+AFTER INSERT AS
+BEGIN
+	DECLARE @productId NVARCHAR(450);
+	SELECT @productId = i.ProductId FROM inserted i;
+	UPDATE Products SET ProductId = UPPER(@productId) WHERE ProductId = @productId;
+END
+GO
+
+
+
+--- Trigger set false for property enable cascase
+GO
 DROP TRIGGER enable_Classification;
 GO
 CREATE TRIGGER enable_Classification
@@ -37,5 +54,72 @@ BEGIN
 	BEGIN
 		UPDATE Categories SET Enable = 0 WHERE ClassificationId = @classificationId AND Enable = 1;
 	END
+END
+GO
+
+GO
+DROP TRIGGER enable_Category;
+GO
+CREATE TRIGGER enable_Category
+ON Categories
+AFTER UPDATE AS
+BEGIN
+	DECLARE @categoryId NVARCHAR(450);
+	SELECT @categoryId = i.CategoryId FROM inserted i;
+	IF (SELECT i.Enable FROM inserted i) = 0 AND (SELECT d.Enable FROM deleted d) = 1
+	BEGIN
+		UPDATE Products SET Enable = 0 WHERE CategoryId = @categoryId AND Enable = 1;
+	END
+END
+GO
+
+
+
+-- Trigger set false property Enable when primay key is setnull
+GO
+DROP TRIGGER enable_Category_When_ClassificationId_Null;
+GO
+CREATE TRIGGER enable_Category_When_ClassificationId_Null
+ON Categories
+AFTER UPDATE AS
+BEGIN
+	DECLARE @categoryId NVARCHAR(450);
+	SELECT @categoryId = i.CategoryId FROM inserted i;
+	IF (SELECT i.ClassificationId FROM inserted i) = NULL AND (SELECT d.Enable FROM deleted d) = 1
+	BEGIN
+		UPDATE Categories SET Enable = 0 WHERE CategoryId = @categoryId;
+	END
+END
+GO
+
+GO
+DROP TRIGGER enable_Product_When_CategoryId_Null;
+GO
+CREATE TRIGGER enable_Product_When_CategoryId_Null
+ON Products
+AFTER UPDATE AS
+BEGIN
+	DECLARE @productId NVARCHAR(450);
+	SELECT @productId = i.ProductId FROM inserted i;
+	IF (SELECT i.CategoryId FROM inserted i) = NULL AND (SELECT d.Enable FROM deleted d) = 1
+	BEGIN
+		UPDATE Products SET Enable = 0 WHERE ProductId = @productId;
+	END
+END
+GO
+
+SELECT DATEADD(hh, 7, GETUTCDATE());
+
+
+-- Trigger set value for ModifyDate of Product
+GO
+DROP TRIGGER set_ModifyDate_Product;
+GO
+CREATE TRIGGER set_ModifyDate_Product
+ON Products
+AFTER UPDATE AS
+BEGIN
+	UPDATE Products SET ModifyDate = DATEADD(hh, 7, GETUTCDATE())
+	WHERE ProductId = (SELECT i.ProductId FROM inserted i);
 END
 GO
