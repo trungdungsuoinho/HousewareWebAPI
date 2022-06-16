c8a32cc96107773bb2d593c0a7b68b1dfc4addcc
trungdungsuoinho <fxdungvusuoinho@gmail.com>
2022-06-17 01:58:25 +0700

    Doing base get order

--- HousewareWebAPI/Models/OrderResponse.cs
+++ HousewareWebAPI/Models/OrderResponse.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HousewareWebAPI.Data.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HousewareWebAPI.Models
 {
@@ -15,5 +17,37 @@ namespace HousewareWebAPI.Models
         public uint TotalFee { get; set; }
         public uint Total { get; set; }
         public DateTime ExpectedDeliveryTime { get; set; }
+    }
+
+    public class GetOrderResponse
+    {
+        public Guid OrderId { get; set; }
+        public string OrderCode { get; set; }
+        public List<ProInCartResponse> Products { get; set; } = new();
+        public uint TotalPrice { get; set; }
+        public uint TotalFee { get; set; }
+        public uint Total { get; set; }
+        public string Status { get; set; }
+        public GetOrderResponse(Order order)
+        {
+            OrderId = order.OrderId;
+            OrderCode = order.OrderCode;
+            foreach (var orderDetail in order.OrderDetails)
+            {
+                Products.Add(new ProInCartResponse
+                {
+                    ProductId = orderDetail.ProductId,
+                    Name = orderDetail.Product.Name,
+                    Avatar = orderDetail.Product.Avatar,
+                    ItemPrice = orderDetail.Product.Price,
+                    Quantity = orderDetail.Quantity,
+                    Price = orderDetail.Product.Price * orderDetail.Quantity
+                });
+            }
+            TotalPrice = (uint)Products.Sum(p => p.Price);
+            Total = order.Amount;
+            TotalFee = Total - TotalPrice;
+            Status = order.OrderStatus;
+        }
     }
 }
